b85438daf867558ac1871e33a16520a3e8a0da24
John Tranier <john.tranier@ticetime.com>
2024-06-09 17:45:59 +0200

    Sketch up Assignment

---
+++ activity-server/src/main/kotlin/org/elaastic/assignment/Assignment.kt
@@ -0,0 +1,11 @@
+package org.elaastic.assignment
+
+import org.elaastic.activity.Activity
+import org.elaastic.directory.Learner
+import org.elaastic.directory.Teacher
+
+class Assignment(
+    activities: List<Activity>,
+    managers: List<Teacher>,
+    registeredLearners: List<Learner>,
+)
