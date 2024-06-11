1c6ab0999d080c5ad1530df0c5af165979a3a0ab
John Tranier <john.tranier@ticetime.com>
2024-06-11 18:53:31 +0200

    Sketch up learners progression

---
+++ activity-server/src/main/kotlin/org/elaastic/activity/sequence/LearnerPhaseState.kt
@@ -0,0 +1,8 @@
+package org.elaastic.activity.sequence
+
+enum class LearnerPhaseState {
+    PENDING,
+    IN_PROGRESS,
+    UNFINISHED,
+    COMPLETED,
+}
--- activity-server/src/main/kotlin/org/elaastic/activity/sequence/SynchronousSequenceActivity.kt
+++ activity-server/src/main/kotlin/org/elaastic/activity/sequence/SynchronousSequenceActivity.kt
@@ -1,6 +1,7 @@
 package org.elaastic.activity.sequence
 
 import org.elaastic.activity.Activity
+import org.elaastic.directory.Learner
 
 class SynchronousSequenceActivity(title: String) : Activity(title) {
     private val phases = listOf<Phase>(
@@ -11,9 +12,12 @@ class SynchronousSequenceActivity(title: String) : Activity(title) {
     private var currentPhaseIndex: Int? = null
     private var closed = false
 
-    fun getPhaseAt(phaseIndex: Int) = phases.getOrNull(phaseIndex) ?: throw IndexOutOfBoundsException()
+    private val learnersProgression = phases.associate { it.id to mutableMapOf<Learner, LearnerPhaseState>() }
 
-    fun getPhase(phaseId: PhaseId) = phases.find { it.id == phaseId }
+    private fun getPhaseAt(phaseIndex: Int) = phases.getOrNull(phaseIndex) ?: throw IndexOutOfBoundsException()
+
+    private fun getPhase(phaseId: PhaseId) =
+        phases.find { it.id == phaseId } ?: throw IllegalArgumentException("The sequence has no phase ${phaseId}")
 
     fun getCurrentPhase(): Phase? = currentPhaseIndex?.let { getPhaseAt(it) }
 
@@ -32,10 +36,13 @@ class SynchronousSequenceActivity(title: String) : Activity(title) {
     fun closePhase() {
         assert(getCurrentPhase()?.isOpen() ?: false) { "The current phase cannot be closed ; it is not open" }
         getCurrentPhase()?.close()
+        // TODO update learners progression
     }
 
     fun nextPhase() {
-        assert(getCurrentPhase()?.isClosed() ?: false) { "Cannot change active phase ; the current phase is not closed" }
+        assert(
+            getCurrentPhase()?.isClosed() ?: false
+        ) { "Cannot change active phase ; the current phase is not closed" }
         assert(getCurrentPhase() != phases.last()) { "Cannot go to next phase ; the active phase is the last one" }
 
         currentPhaseIndex = currentPhaseIndex?.plus(1)
@@ -46,5 +53,17 @@ class SynchronousSequenceActivity(title: String) : Activity(title) {
     }
 
     fun getActivePhases() = phases.filter { phase -> phase.isOpen() }
+
+    fun startPhaseForLearner(learner: Learner, phaseId: PhaseId) {
+        val phase = getPhase(phaseId)
+        assert(phase.isOpen()) { "The phase $phaseId is not open" }
+
+        learnersProgression[phaseId]!![learner] = LearnerPhaseState.IN_PROGRESS
+    }
+
+    fun completePhaseForLearner(learner: Learner, phaseId: PhaseId) {
+
+        // TODO It must be the current phase for the learner...
+    }
 
 }
